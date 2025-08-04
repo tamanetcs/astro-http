ca329f4f827919415be32e2335a835ac19896aa7
tamanetcs <tamanetcs@gmail.com>
2025-08-04 17:32:06 -0600

    feat: enhance LikeCounter component with API integration for fetching and updating likes

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineOptions({ name: 'LikeCounter' })
+interface Props {
+    postId: string
+}
+const props = defineProps<Props>()
+// console.log(props.postId)
+const likeCount = ref(0)
+const likeClicks = ref(0)
+const isLoading = ref(true)
 const likePost = () => {
     console.log('+1 like')
 }
+const getCurrentLikes = async () => {
+    const resp = await fetch(`/api/posts/likes/${props.postId}`)
+    if (!resp.ok) return
+    const data = await resp.json()
+    likeCount.value = data.likes
+    isLoading.value = false
+}
+getCurrentLikes()
 </script>
 
 <template>
-    <button @click="likePost">Like Counter</button>
+    <div v-if="isLoading">Loading...</div>
+    <button v-else-if="likeCount === 0" @click="likePost">Like this post</button>
+    <button v-else="likeCount === 0" @click="likePost">
+        Likes
+        <span>{{ likeCount }}</span>
+    </button>
 </template>
 
 <style scoped>
